1e7ff9ae449542bb070a7528d5d62c329e82f260
0xShellcode <BDAthans@yahoo.com>
2019-07-03 11:38:33 -0500

    Add files via upload

---
+++ EnumerateSQLOnNetwork.ps1
@@ -0,0 +1,14 @@
+﻿#Enumerates all SQL instances running on network
+
+$username = $env:USERNAME
+$outfile = "C:\Users\$username\Desktop\EnumSQL.log"
+
+clear
+Write-Output("`nEnumeratoring SQL instances running on the network... `n")
+
+[System.Data.Sql.SqlDataSourceEnumerator]::Instance.GetDataSources()
+Write-Output("`nWriting Output to File:", $outfile)
+[System.Data.Sql.SqlDataSourceEnumerator]::Instance.GetDataSources() | Out-File -FilePath $outfile
+
+Write-Output("`n")
+pause
